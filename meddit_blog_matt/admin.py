2c73a23ca4fab3311b7b0c94320ff627d1b4280a
Matex600 <mat.leks@gmail.com>
2022-02-02 16:53:11 +0000

    Rename CreatePost > Post, make migrations and migrate

--- meddit_blog_matt/admin.py
+++ meddit_blog_matt/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import CreatePost
+from .models import Post
 
-admin.site.register(CreatePost)
+admin.site.register(Post)
--- meddit_blog_matt/models.py
+++ meddit_blog_matt/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class CreatePost(models.Model):
+class Post(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
