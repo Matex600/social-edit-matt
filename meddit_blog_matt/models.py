10a15671f3274f63c0a909722a30d4739c7f3403
Matex600 <mat.leks@gmail.com>
2022-02-02 16:05:21 +0000

    Create class in models.py CreatePost, for db use and add to admin.py

--- meddit_blog_matt/models.py
+++ meddit_blog_matt/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class CreatePost(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title + ' | ' + self.author
