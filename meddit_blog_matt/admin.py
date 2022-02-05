48a2b4713040902db6b29736220b22ff3e2303b9
Matex600 <mat.leks@gmail.com>
2022-02-05 12:13:05 +0000

    Add Blog admin class and comment class for admin page

--- meddit_blog_matt/admin.py
+++ meddit_blog_matt/admin.py
@@ -1,4 +1,30 @@
 from django.contrib import admin
-from .models import Post
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Post, Comment
 
-admin.site.register(Post)
+
+
+@admin.register(Post)
+class BlogAdmin(SummernoteModelAdmin):
+    """
+    Class for creating blog post in admin view
+    """
+    list_display = ('title', 'slug', 'status', 'date_created')
+    search_fields = ['title', 'body']
+    list_filter = ('status', 'date_created')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('body')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """
+    Class for creating comments in admin view
+    """
+    list_display = ('name', 'content', 'post', 'date_created', 'approved')
+    list_filter = ('approved', 'date_created')
+    search_fields = ('name', 'email', 'content')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.updated(approved=True)
