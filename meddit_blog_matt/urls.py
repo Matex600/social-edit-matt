fe54ecce3ba81df5614cb0d4be4697cd200e2477
Matex600 <mat.leks@gmail.com>
2022-02-09 23:16:57 +0000

    Add blogdetailview path to urls.py

--- meddit_blog_matt/urls.py
+++ meddit_blog_matt/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import MainView
+from .views import MainView, BlogDetailView
 
 urlpatterns = [
     path('', MainView.as_view(), name='home'),
+    path('blog/<int:pk>', BlogDetailView.as_view(), name='blog-detail'),
 ]
