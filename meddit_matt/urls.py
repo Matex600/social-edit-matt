b03de558c1afc25c99e62bf763d5ef9b0af3af1d
Matex600 <mat.leks@gmail.com>
2022-02-09 15:34:51 +0000

    Add , to path

--- meddit_matt/urls.py
+++ meddit_matt/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('meddit_blog_matt.urls')),
-    path('summernote/', include('django_summernote.urls'))
+    path('', include('meddit_blog_matt.urls'), name='meddit_blog_matt.urls'),
+    path('summernote/', include('django_summernote.urls')),
 ]
