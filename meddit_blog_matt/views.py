f201defd0561636b85077621b73f9dbf11ff2985
Matex600 <mat.leks@gmail.com>
2022-02-09 23:17:43 +0000

    Add query set to mainview and add blogdetail view

--- meddit_blog_matt/views.py
+++ meddit_blog_matt/views.py
@@ -4,5 +4,18 @@ from .models import Post
 
 
 class MainView(ListView):
+    """
+    View for index.html PLACEHOLDER
+    """
     model = Post
-    template_name = 'home.html'
+    template_name = 'index.html'
+    queryset = Post.objects.filter(status=1).order_by('-date_created')
+    paginate_by = 4
+
+
+class BlogDetailView(DetailView):
+    """
+    View for individual pages for each post
+    """
+    model = Post
+    template_name = 'blog_details.html'
