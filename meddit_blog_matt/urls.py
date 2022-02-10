60bd7ccadf1812363bc85a040d3b93f8ce23d938
Matex600 <mat.leks@gmail.com>
2022-02-10 20:49:33 +0000

    Create path add_post to template

--- meddit_blog_matt/urls.py
+++ meddit_blog_matt/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import MainView, BlogDetailView
+from .views import MainView, BlogDetailView, AddPostView
 
 urlpatterns = [
     path('', MainView.as_view(), name='home'),
     path('blog/<int:pk>', BlogDetailView.as_view(), name='blog-detail'),
+    path('add_post/', AddPostView.as_view(), name='add_post'),
 ]
