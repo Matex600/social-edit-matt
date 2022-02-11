7a6db88ceea5561dac0771b691dff5945d7d23f6
Matex600 <mat.leks@gmail.com>
2022-02-11 15:54:57 +0000

    Add edit post to forms.py to edit posts

--- meddit_blog_matt/forms.py
+++ meddit_blog_matt/forms.py
@@ -19,3 +19,20 @@ class AddPostForm(forms.ModelForm):
             'body': forms.Textarea(attrs={'class': 'form-control'}),
             'post_image': forms.ClearableFileInput(attrs={'class': 'form-control'}),
         }
+
+
+class EditPostForm(forms.ModelForm):
+    """
+    Form class for my create post functionality PLACEHOLDER
+    """
+    class Meta:
+        """
+        Form class for my create post functionality PLACEHOLDER
+        """
+        model = Post
+        fields = ('title', 'title_tag', 'body', 'status')
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+        }
