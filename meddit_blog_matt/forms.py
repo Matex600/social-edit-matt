991cea297349979a3cdec1b32d76a915a5c2bcf3
Matex600 <mat.leks@gmail.com>
2022-02-11 15:10:37 +0000

    Add forms.py to control create blog post using widget

---
+++ meddit_blog_matt/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from .models import Post
+
+
+class AddPostForm(forms.ModelForm):
+    """
+    Form class for my create post functionality PLACEHOLDER
+    """
+    class Meta:
+        """
+        Form class for my create post functionality PLACEHOLDER
+        """
+        model = Post
+        fields = ('title', 'title_tag', 'author', 'body', 'post_image', 'status')
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Choose a blog title!'}),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+            'author': forms.Select(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+            'post_image': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+        }
