10680b1264b1fcce60a775fd645c828a4ced702b
Matex600 <mat.leks@gmail.com>
2022-02-11 15:55:43 +0000

    Added url for edit post view

--- meddit_blog_matt/urls.py
+++ meddit_blog_matt/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import MainView, BlogDetailView, AddPostView
+from .views import MainView, BlogDetailView, AddPostView, EditPostView
 
 urlpatterns = [
     path('', MainView.as_view(), name='home'),
     path('blog/<int:pk>', BlogDetailView.as_view(), name='blog-detail'),
     path('add_post/', AddPostView.as_view(), name='add_post'),
+    path('article/edit/<int:pk>', EditPostView.as_view(), name='edit_post')
 ]
