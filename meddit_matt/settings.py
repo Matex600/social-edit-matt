26b8a786d41802fb4c52511925def747ea98f058
Matex600 <mat.leks@gmail.com>
2022-02-12 16:13:26 +0000

    Add redirect for login/logout to settings.py

--- meddit_matt/settings.py
+++ meddit_matt/settings.py
@@ -137,6 +137,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
