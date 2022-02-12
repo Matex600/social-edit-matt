30cdd4f34139ba6986c1070650908dea9f85931b
Matex600 <mat.leks@gmail.com>
2022-02-12 14:11:00 +0000

    Connect new app to project urls

--- meddit_matt/urls.py
+++ meddit_matt/urls.py
@@ -4,5 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('meddit_blog_matt.urls'), name='meddit_blog_matt.urls'),
+    path('users/', include('django.contrib.auth.urls')),
+    path('users/', include('meddit_users'), name='meddit_users'),
     path('summernote/', include('django_summernote.urls')),
 ]
