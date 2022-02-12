16f917d02ce8f73079a340f28cc6ec0007d98cb9
Matex600 <mat.leks@gmail.com>
2022-02-12 11:13:37 +0000

    Import DeleteView add path to DeletePostView, add comma to edit path

--- meddit_blog_matt/urls.py
+++ meddit_blog_matt/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import MainView, BlogDetailView, AddPostView, EditPostView
+from .views import MainView, BlogDetailView, AddPostView, EditPostView, DeletePostView
 
 urlpatterns = [
     path('', MainView.as_view(), name='home'),
     path('blog/<int:pk>', BlogDetailView.as_view(), name='blog-detail'),
     path('add_post/', AddPostView.as_view(), name='add_post'),
-    path('article/edit/<int:pk>', EditPostView.as_view(), name='edit_post')
+    path('article/edit/<int:pk>', EditPostView.as_view(), name='edit_post'),
+    path('article/<int:pk>/delete', DeletePostView.as_view(), name='delete_post'),
 ]
