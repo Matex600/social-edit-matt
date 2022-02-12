81db2a39226f5b3ebf856c620dbcc9f7ca8d2af2
Matex600 <mat.leks@gmail.com>
2022-02-12 14:58:25 +0000

    Fix typo that caused an circular import error

--- meddit_matt/urls.py
+++ meddit_matt/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('meddit_blog_matt.urls'), name='meddit_blog_matt.urls'),
     path('users/', include('django.contrib.auth.urls')),
-    path('users/', include('meddit_users'), name='meddit_users'),
+    path('users/', include('meddit_users.urls'), name='meddit_users'),
     path('summernote/', include('django_summernote.urls')),
 ]
