c5ee6ed0eb87f0e6ddc9723fe666c0fa5193630a
Matex600 <mat.leks@gmail.com>
2022-02-13 21:32:14 +0000

    Add category url to urls.py

--- meddit_blog_matt/urls.py
+++ meddit_blog_matt/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import MainView, BlogDetailView, AddPostView, EditPostView, DeletePostView
+from .views import MainView, BlogDetailView, AddPostView, EditPostView, DeletePostView, AddCategoryView
 
 urlpatterns = [
     path('', MainView.as_view(), name='home'),
     path('blog/<int:pk>', BlogDetailView.as_view(), name='blog-detail'),
     path('add_post/', AddPostView.as_view(), name='add_post'),
+    path('add_category/', AddCategoryView.as_view(), name='add_category'),
     path('article/edit/<int:pk>', EditPostView.as_view(), name='edit_post'),
     path('article/<int:pk>/delete', DeletePostView.as_view(), name='delete_post'),
 ]
