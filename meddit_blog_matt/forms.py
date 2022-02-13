3c9017b87ddecc05d521f2ea5cbcd8bc8d674615
Matex600 <mat.leks@gmail.com>
2022-02-13 21:00:10 +0000

    Add category drop down to form with list to make selection more dynamic

--- meddit_blog_matt/forms.py
+++ meddit_blog_matt/forms.py
@@ -1,5 +1,12 @@
 from django import forms
-from .models import Post
+from .models import Post, Category
+
+choices = Category.objects.all().values_list('name' ,'name')
+
+choice_list = []
+
+for item in choices:
+    choice_list.append(item)
 
 
 class AddPostForm(forms.ModelForm):
@@ -11,11 +18,12 @@ class AddPostForm(forms.ModelForm):
         Form class for my create post functionality PLACEHOLDER
         """
         model = Post
-        fields = ('title', 'title_tag', 'author', 'body', 'post_image', 'status')
+        fields = ('title', 'title_tag', 'author', 'category', 'body', 'post_image', 'status')
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Choose a blog title!'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
+            'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
             'post_image': forms.ClearableFileInput(attrs={'class': 'form-control'}),
         }
@@ -30,9 +38,10 @@ class EditPostForm(forms.ModelForm):
         Form class for my create post functionality PLACEHOLDER
         """
         model = Post
-        fields = ('title', 'title_tag', 'body', 'status')
+        fields = ('title', 'title_tag', 'category', 'body', 'status')
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+            'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
         }
