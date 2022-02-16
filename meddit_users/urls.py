246bc08e45c7cbed03b043fbaf260cdfbe3dbe76
Matex600 <mat.leks@gmail.com>
2022-02-16 13:57:42 +0000

    Add basic user edit button with functionality to navb

--- meddit_users/urls.py
+++ meddit_users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UserRegisterView
+from .views import UserRegisterView, UserUpdateView
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
+    path('update_profile/', UserUpdateView.as_view(), name='update_profile'),
 ]
--- meddit_users/views.py
+++ meddit_users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.urls import reverse_lazy
 from .forms import RegisterForm
 
@@ -11,3 +11,15 @@ class UserRegisterView(generic.CreateView):
     form_class = RegisterForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
+
+
+class UserUpdateView(generic.UpdateView):
+    """
+    View for registering users using django forms
+    """
+    form_class = UserChangeForm
+    template_name = 'registration/edit_user.html'
+    success_url = reverse_lazy('login')
+
+    def get_object(self):
+        return self.request.user
