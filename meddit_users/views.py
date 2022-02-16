5ffaf4e24d3d34c9582d71e1d43dc833c6dd5d19
Matex600 <mat.leks@gmail.com>
2022-02-16 01:53:24 +0000

    Create form.py class for register, update views.py

--- meddit_users/views.py
+++ meddit_users/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-
+from .forms import RegisterForm
 
 class UserRegisterView(generic.CreateView):
     """
     View for registering users using django forms
     """
-    form_class = UserCreationForm
+    form_class = RegisterForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
