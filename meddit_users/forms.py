eadad10036b0bd6bece6842f74c2d680d7d910d5
Matex600 <mat.leks@gmail.com>
2022-02-16 16:23:46 +0000

    New form for updating passwords fields and adding style

--- meddit_users/forms.py
+++ meddit_users/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth.models import User
 from django import forms
 
@@ -50,3 +50,19 @@ class EditUserForm(UserChangeForm):
         """
         model = User
         fields = ('username', 'first_name', 'last_name', 'date_joined', 'last_login')
+
+
+class PasswordUpdatingForm(PasswordChangeForm):
+    """
+    Form used to style fields for registering a user to the blog page
+    """
+    old_password = forms.CharField(label='Old Password', widget=forms.PasswordInput(attrs={'class': 'form-control', 'type':'password'}))
+    new_password1 = forms.CharField(label='New Password', widget=forms.PasswordInput(attrs={'class': 'form-control', 'type':'password'}))
+    new_password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput(attrs={'class': 'form-control', 'type':'password'}))
+
+    class Meta:
+        """
+        Display fields for registering user
+        """
+        model = User
+        fields = ('old_password', 'new_password1', 'new_password2')
