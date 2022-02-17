4f569c99625350604068056138565d275120ab2d
Matex600 <mat.leks@gmail.com>
2022-02-17 17:13:01 +0000

    Add links and image to userprofile, migrate

--- meddit_users/models.py
+++ meddit_users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from cloudinary.models import CloudinaryField
 
 class UserProfile(models.Model):
     """
@@ -8,7 +8,13 @@ class UserProfile(models.Model):
     """
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     user_bio = models.TextField()
-
+    user_image = CloudinaryField('image', null=True, blank=True)
+    github_url = models.CharField(max_length=225, null=True, blank=True)
+    linkedin_url = models.CharField(max_length=225, null=True, blank=True)
+    facebook_url = models.CharField(max_length=225, null=True, blank=True)
+    twitter_url = models.CharField(max_length=225, null=True, blank=True)
+    instagram_url = models.CharField(max_length=225, null=True, blank=True)
+    
     def __str__(self):
         return f"{self.user}"
 
