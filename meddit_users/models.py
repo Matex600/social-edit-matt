7468a017687fe08ba68f364d509361bbb0422a6f
Matex600 <mat.leks@gmail.com>
2022-02-17 13:06:49 +0000

    Add basic user model and admin register, make migration

--- meddit_users/models.py
+++ meddit_users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    """
+    User model 
+    """
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    user_bio = models.TextField()
+
+    def __str__(self):
+        return f"{self.user}"
+
