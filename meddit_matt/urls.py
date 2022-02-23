ab34537d3f0ddf45a23f6477d805de9840714714
Matex600 <mat.leks@gmail.com>
2022-02-23 22:24:27 +0000

    Add automatic DEBUG option in settings as per tutor support

--- meddit_matt/urls.py
+++ meddit_matt/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,4 +9,4 @@ urlpatterns = [
     path('users/', include('django.contrib.auth.urls')),
     path('users/', include('meddit_users.urls'), name='meddit_users'),
     path('summernote/', include('django_summernote.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
