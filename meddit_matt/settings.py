5b00369f5f7bb91bf21a34b8ff50290e5c8d9061
Matex600 <mat.leks@gmail.com>
2022-02-25 00:57:31 +0000

    Add context to templates > options, commit missed migration file

--- meddit_matt/settings.py
+++ meddit_matt/settings.py
@@ -80,6 +80,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'meddit_blog_matt.context.categories_processor',
             ],
         },
     },
