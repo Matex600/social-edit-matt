d3e79dc9c80aa1fa7f90c94c345472b2f6977a96
Matex600 <mat.leks@gmail.com>
2022-02-25 00:55:56 +0000

    Update urls.py with changes to categories.view

--- meddit_blog_matt/urls.py
+++ meddit_blog_matt/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path('add_category/', AddCategoryView.as_view(), name='add_category'),
     path('blog/edit/<int:pk>', EditPostView.as_view(), name='edit_post'),
     path('blog/<int:pk>/delete', DeletePostView.as_view(), name='delete_post'),
-    path('category/<str:cate>/', category_view, name='category'),
+    path('category/<int:id>/', category_view, name='category'),
     path('category-list/', category_list_view, name='category-list'),
     path('like/<int:pk>', like_view, name='up_vote_post'),
     path('blog/<int:pk>/comment/',
